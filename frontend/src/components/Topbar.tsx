0e91f1f336a2dc1bbf363c5ff400d9846b7c7f2c
Saksham Khare <sakshamkhare19@gmail.com>
2024-11-04 23:16:42 +0530

    ugly code of navbar

---
+++ frontend/src/components/Topbar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Topbar = () => {
+  return (
+    <div className="flex justify-center">
+      <div className="max-w-screen-lg bg-black min-h-56 align-middle">
+        Nav bar
+      </div>
+    </div>
+  );
+};
+
+export default Topbar;
